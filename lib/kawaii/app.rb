00ca6c7bbff16bc41d3f6e508ade78ba30cfe488
rybex <tomek.rybka@gmail.com>
2016-05-16 10:47:43 +0800

    Added processing request to App class. We get route from Router and then call returned handler. If route doesn't exist we call error handler

--- lib/kawaii/app.rb
+++ lib/kawaii/app.rb
@@ -2,6 +2,46 @@ module Kawaii
   class App
 
     def call(env)
+      self.class.process_request(env)
+    rescue => error
+      self.class.handle_exception(error)
+    end
+
+    class << self
+
+      def use(middleware, *args, &block)
+        rack_builder.use(middleware, *args, &block)
+      end
+
+      def custom_exception(&block)
+        @exception_handler = block
+      end
+
+      def process_request(env)
+        route = router.match(env)
+        rack_builder.run route.handler
+        handler = rack_builder.to_app
+        handler.call(env)
+      end
+
+      def rack_builder
+        @rack_builder ||= Rack::Builder.new
+      end
+
+      def router
+        @router ||= Kawaii::Router.new
+      end
+
+      def handle_exception(error)
+        handler = @exception_handler || error_handler(error)
+        handler.call(error)
+      end
+
+      private
+
+      def error_handler(error)
+        Proc.new (error) { raise error }
+      end
     end
   end
 end
