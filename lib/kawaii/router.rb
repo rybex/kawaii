f2b5dcf41f9776a3de4f2c3b62b7ada9c12ba77c
rybex <tomek.rybka@gmail.com>
2016-05-16 10:44:12 +0800

    Added router implementation with test. I decided to split logic of creating routes (Routes class) and finding route (Router)

---
+++ lib/kawaii/router.rb
@@ -0,0 +1,34 @@
+module Kawaii
+  class Router
+
+    def initialize
+      @routes = Kawaii::Routing::Routes.routes
+    end
+
+    def match(env)
+      searched_route = create_route(env)
+      route          = find_route(searched_route)
+      raise RouteNotFound unless route
+      route
+    end
+
+    private
+    attr_reader :routes
+
+    def find_route(searched_route)
+      routes.detect {|route| route == searched_route }
+    end
+
+    def create_route(env)
+      Kawaii::Routing::Route.new(request_path(env), request_method(env))
+    end
+
+    def request_path(env)
+      env['REQUEST_PATH']
+    end
+
+    def request_method(env)
+      env['REQUEST_METHOD'].to_sym
+    end
+  end
+end
---
+++ spec/router_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+
+module Kawaii
+  describe Router do
+
+    let(:router)          { Router.new }
+    let(:correct_request) { {'REQUEST_PATH' => '/test_path', 'REQUEST_METHOD' => 'GET'} }
+    let(:wrong_request)   { {'REQUEST_PATH' => '/wrong_path', 'REQUEST_METHOD' => 'GET'} }
+
+    before do
+      Class.new(Kawaii::Routing::Routes) do
+        get '/test_path', 'test#method'
+      end
+    end
+
+    after do
+      Kawaii::Routing::Routes.reset
+    end
+
+    it 'return route specified in Routes class' do
+      route = router.match(correct_request)
+
+      expect(route).to_not be         nil
+      expect(route.http_method).to eq :GET
+      expect(route.mapping).to eq     'test#method'
+      expect(route.path).to eq        '/test_path'
+    end
+
+    it 'raise exception for not existing route' do
+      expect {
+        router.match(wrong_request)
+      }.to raise_error(Kawaii::RouteNotFound, 'Please define route in routes file')
+    end
+
+  end
+end
